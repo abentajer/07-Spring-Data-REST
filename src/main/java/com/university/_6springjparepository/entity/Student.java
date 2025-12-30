55a170831b7a86dbcdc6f63fd76535ce3845f120
Ahmed Bentajer <a.bentajer@gmail.com>
2025-12-30 13:44:04 +0100

    Spring Data Rest

---
+++ src/main/java/com/university/_6springjparepository/entity/Student.java
@@ -0,0 +1,73 @@
+package com.university._6springjparepository.entity;
+
+import jakarta.persistence.*;
+
+@Entity // 1. Marks this class as a database entity
+@Table(name = "student") // 2. (Optional) Customizes the table name in DB
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String fistName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(unique = true, name = "email")
+    private String email;
+
+    public Student() {
+
+    }
+
+    public Student(String fistName, String lastName, String email) {
+        this.fistName = fistName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFistName() {
+        return fistName;
+    }
+
+    public void setFistName(String fistName) {
+        this.fistName = fistName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", fistName='" + fistName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/university/_6springjparepository/repository/StudentRepository.java
@@ -0,0 +1,29 @@
+package com.university._6springjparepository.repository;
+
+import com.university._6springjparepository.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+// 1. ANNOTATION: customize the path.
+// Default would be "/students", but here we can be explicit.
+// collectionResourceRel = key in JSON ("students": [...])
+// path = URL path segment (localhost:8080/api/members)
+@RepositoryRestResource(collectionResourceRel = "students", path = "students")
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    // 2. EXPOSING QUERY METHODS
+    // Spring Data REST automatically finds this and exposes it at:
+    // URL: /students/search/findByEmail?email=...
+    // We use @Param to name the query parameter in the URL
+    Student findByEmail(@Param("email") String email);
+
+    // 3. HIDING METHODS
+    // If you want to prevent DELETE operations via the API, you can override and hide them.
+    @Override
+    @RestResource(exported = false) // This method will NOT be exposed via REST
+    void deleteById(Integer id);
+}
